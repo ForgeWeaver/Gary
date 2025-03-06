029f60ac6c3319ed41286b28e0170d3fda78ca31
ForgeWeaver <200391320+ForgeWeaver@users.noreply.github.com>
2025-03-06 20:59:30 +0000

    Clone Whirlpool Repositioning Bot

---
+++ src/wallet.rs
@@ -0,0 +1,12 @@
+// Modified from: https://github.com/orca-so/whirlpools/tree/main/examples/rust-sdk/whirlpool_repositioning_bot
+// Original: https://github.com/orca-so/whirlpools
+
+use solana_sdk::{signature::Keypair, signer::Signer};
+use std::fs;
+
+pub fn load_wallet() -> Box<dyn Signer> {
+    let wallet_string = fs::read_to_string("wallet.json").unwrap();
+    let keypair_bytes: Vec<u8> = serde_json::from_str(&wallet_string).unwrap();
+    let wallet = Keypair::from_bytes(&keypair_bytes).unwrap();
+    Box::new(wallet)
+}
